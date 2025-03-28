22b5f313f854c8adad8e19261dc0941954deb364
Tanujeet <singhtanujeet@gmail.com>
2025-03-28 09:16:52 +0530

    Updated Frontend and backend

--- todo-react/src/Components/CreateTodo.jsx
+++ todo-react/src/Components/CreateTodo.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useState } from "react";
 
 const CreateTodo = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
       <input
@@ -9,6 +13,10 @@ const CreateTodo = () => {
         style={{
           padding: 10,
           margin: 10,
+        }}
+        onChange={function (e) {
+          const value = e.target.value;
+          setTitle(e.target.value);
         }}
       />
       <br />
@@ -19,12 +27,31 @@ const CreateTodo = () => {
           padding: 10,
           margin: 10,
         }}
+        onChange={function (e) {
+          const value = e.target.value;
+          setDescription(e.target.value);
+        }}
       />
       <br />
       <button
         style={{
           padding: 10,
           margin: 10,
+        }}
+        onClick={() => {
+          fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }).then(async function (res) {
+            const json = await res.json();
+            alert("Todo added");
+          });
         }}
       >
         Add a Todo
