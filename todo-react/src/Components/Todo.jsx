0602a5e4197c0845918143a96e48a7d77bd463bb
Tanujeet <singhtanujeet@gmail.com>
2025-03-28 09:45:19 +0530

    Updated Frontend and backend

--- todo-react/src/Components/Todo.jsx
+++ todo-react/src/Components/Todo.jsx
@@ -4,14 +4,14 @@ const Todo = ({ todos = [], toggleComplete }) => {
   return (
     <div>
       {todos.map((todo) => (
-        <div key={todo.id} style={{ marginBottom: "10px" }}>
+        <div key={todo._id} style={{ marginBottom: "10px" }}>
           <h1
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
           >
             {todo.title}
           </h1>
           <h2>{todo.description}</h2>
-          <button onClick={() => toggleComplete(todo.id)}>
+          <button onClick={() => toggleComplete(todo._id)}>
             {todo.completed ? "✅ Completed" : "Mark as completed"}
           </button>
         </div>
