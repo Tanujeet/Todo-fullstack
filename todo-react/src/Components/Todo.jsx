c43ecd34c556b21f291a9a3a4f57d954920c81ff
Tanujeet <singhtanujeet@gmail.com>
2025-03-28 09:26:57 +0530

    Updated Frontend and backend

--- todo-react/src/Components/Todo.jsx
+++ todo-react/src/Components/Todo.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-const Todo = ({ todos = [] }) => {
+const Todo = ({ todos = [], toggleComplete }) => {
   return (
     <div>
       {todos.map((todo, index) => (
         <div key={index}>
           <h1>{todo.title}</h1>
           <h2>{todo.description}</h2>
-          <button>{todo.completed ? "Completed" : "Mark as completed"}</button>
+          <button
+            onClick={() => {
+              toggleComplete(index);
+            }}
+          >
+            {todo.completed ? "Completed" : "Mark as completed"}
+          </button>
         </div>
       ))}
     </div>
